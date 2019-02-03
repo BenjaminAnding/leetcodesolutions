254808d674b6d4239448e379357654b9acc01ec7
Benjamin Anding <c00171534@louisiana.edu>
2019-02-03 03:11:37 -0600

    Sum of Even Numbers After Queries solution

---
+++ SumofEvenNumbersAfterQueries.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def sumEvenAfterQueries(self, A, queries):
+        """
+        :type A: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        for i in queries:
+            A[i[1]] += i[0]
+        total = 0
+        for i in A:
+            if i % 2 == 0:
+                total += i
+        return total
